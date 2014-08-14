ccacaf3bcdd536b16985a9c562b0e2fa2bff4a93
Noel Miller <m673639@a4440486.federated.fds>
2014-08-14 16:46:57 -0700

    gerrit projects report added

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ devops-dashboard
 ================
 
 One stop shop for status of all your DevOps tools
+
+** Here's how you use curl to do REST calls with supported services
+
+Gerrit
+curl --digest --user $GERRIT_USER:$GERRIT_PASS  $GERRIT_URL/a/projects/
+
+
+** Supported Services
+[] Gerrit
+[] Jenkins
+
+
---
+++ lib/Reporter.rb
@@ -0,0 +1,28 @@
+# Reporter sloth-finder library that creates reports.
+class Reporter
+
+  #attr_accessor :app_id, :type
+
+# custom libs
+BASEDIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$: << File.join(BASEDIR, "lib")
+  require "GerritDoorman"
+
+  def initialize(gerrit_url, gerrit_user, gerrit_pass)
+    @gerrit_url   = gerrit_url
+    @gerrit_user  = gerrit_user
+    @gerrit_pass  = gerrit_pass
+  end
+
+  # projects report generator
+  def projects
+    $LOG.info("Projects list")
+    gerrit_lexicon          = GerritDoorman.new(@gerrit_url, @gerrit_user, @gerrit_pass)
+    gerrit_lexicon.type     = "projects"
+    proj_obj                = gerrit_lexicon.door
+    proj_obj.each do |proj|
+      puts proj
+    end
+  end
+
+end
