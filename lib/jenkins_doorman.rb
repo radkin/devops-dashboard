067d196999c47caab319fc0bc6251b95d0df37aa
Noel Miller <m673639@a4440486.federated.fds>
2014-09-12 11:59:18 -0700

    big refactor with some limited style changes. Mostly, keeping classes aware of only the variables they need to know about here.

--- lib/jenkins_doorman.rb
+++ lib/jenkins_doorman.rb
@@ -1,6 +1,6 @@
 # devops-dashboard library that opens doors to the Jenkins API.
 class JenkinsDoorman
-  attr_accessor :jenkins_user, :jenkins_pass, :jenkins_url
+  attr_accessor :jenkins_params
 
   require 'uri'
   require 'net/http'
@@ -11,11 +11,14 @@ class JenkinsDoorman
 
   # door, standard request, nothing fancy, requires no special information
   def door
-    $LOG.info("URL is #{@jenkins_url}/api/json")
-    uri               = URI.parse "#{@jenkins_url}/api/json"
+    jenkins_user      = @jenkins_params[0]
+    jenkins_pass      = @jenkins_params[1]
+    jenkins_url       = @jenkins_params[2]
+    $LOG.info("URL is #{jenkins_url}/api/json")
+    uri               = URI.parse "#{jenkins_url}/api/json"
     http              = Net::HTTP.new(uri.host, uri.port)
     request           = Net::HTTP::Post.new(uri.request_uri)
-    request.basic_auth("#{@jenkins_user}", "#{@jenkins_pass}")
+    request.basic_auth("#{jenkins_user}", "#{jenkins_pass}")
     response          = http.request(request)
     $LOG.debug('RAW response is ...')
     $LOG.debug(response)
