5ecdb04e4c51a530571dc25c3f0c639a46499de2
Noel Miller <m673639@a4440486.federated.fds>
2015-01-02 15:08:38 -0800

    created a new view for jenkins jobs drill down information. At the moment it is just displaying jenkins masters because we still need a solid REGEX for mysql that will pull all available data by server. Furthermore, there will have to be some kind of logic that allows us to track the difference between historical data and the most recent info

--- ddash/app/controllers/jenkins_hello_controller.rb
+++ ddash/app/controllers/jenkins_hello_controller.rb
@@ -1,4 +1,4 @@
-class ReportsController < ApplicationController
+class JenkinsHelloController < ApplicationController
   require 'resque'
   require_dependency 'reporter'
   require_dependency 'jenkins_jobs_objects'
@@ -25,21 +25,6 @@ class ReportsController < ApplicationController
       @all_status.push(*status)
     end
   end
-end
-
-=begin
-  def show_job_details
-    masters           = JenkinsMasters.new
-    @jenkins_masters  = masters.generate
-    @jenkins_masters.each do |master|
-      job_list  = JenkinsInfo.new
-      job_list.master = master
-      jobs = job_list.generate
-      gen_report    = Reporter.new
-      jobs.each do |job|
-        gen_report.jenkins_job_name = job
-        @details_report = gen_report.job_details
-      end
-    end
+  def show
   end
-=end
+end
---
+++ ddash/app/controllers/jenkins_jobs_controller.rb
@@ -0,0 +1,4 @@
+class JenkinsJobsController < ApplicationController
+  def index
+  end
+end
