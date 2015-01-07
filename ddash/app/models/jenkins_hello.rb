eb821d53de991af12a516bacaeffd7d5e733dc1a
Noel Miller <m673639@a4440486.federated.fds>
2015-01-07 15:24:16 -0800

    we now have jenkins_jobs and jenkins_hello as ligit routes. The jenkins_hello will show a condensed report for blue & red jobs. jenkins_jobs shows blue and red jobs in a list

--- ddash/app/models/jenkins_hello.rb
+++ ddash/app/models/jenkins_hello.rb
@@ -1,2 +1,8 @@
 class JenkinsHello < ActiveRecord::Base
+  #scope :by_master, -> { where(:url => /(buildserver)/) }
+  scope :by_color_red, -> { where(color: 'red') }
+  scope :by_color_blue, -> { where(color: 'blue') }
+  # this doesn't work because datetime has the time of day while this is looking for date only.
+  #scope :today, lambda { where(created_at: Date.today) }
+  scope :uniq_job, lambda { select(:name, :url).uniq }
 end
