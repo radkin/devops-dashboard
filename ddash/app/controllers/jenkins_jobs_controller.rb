5f083264d62d0786aa568cf0fd316fd1ab4af3b9
Noel Miller <m673639@a4440486.federated.fds>
2015-01-08 15:38:42 -0800

    added pie graph with status for all jobs

--- ddash/app/controllers/jenkins_jobs_controller.rb
+++ ddash/app/controllers/jenkins_jobs_controller.rb
@@ -1,8 +1,16 @@
 class JenkinsJobsController < ApplicationController
   require 'googlecharts'
   def index
-    @red_jobs     = JenkinsHello.by_color_red.uniq_job
-    @blue_jobs    = JenkinsHello.by_color_blue.uniq_job
-    @pie_chart    = Gchart.pie(:data => [@red_jobs.count,@blue_jobs.count], :title => 'Red vs Blue jobs', :size => '400x200', :labels =>     ['red', 'blue'])
+    # jobs
+    @red_jobs       = JenkinsHello.by_color_red.uniq_job
+    @blue_jobs      = JenkinsHello.by_color_blue.uniq_job
+    @yellow_jobs    = JenkinsHello.by_color_yellow.uniq_job
+    @grey_jobs      = JenkinsHello.by_color_grey.uniq_job
+    @disabled_jobs  = JenkinsHello.by_color_disabled.uniq_job
+    @notbuilt_jobs  = JenkinsHello.by_color_notbuilt.uniq_job
+    @aborted_jobs   = JenkinsHello.by_color_aborted.uniq_job
+    # charts
+    @total_job_status_pie_chart = Gchart.pie(:data => [@red_jobs.count,@blue_jobs.count,@yellow_jobs.count,@grey_jobs.count,@disabled_jobs.count,@notbuilt_jobs.count,@aborted_jobs.count], :title => 'Total job status', :size => '600x300', :labels =>     ['red','blue','yellow','grey','disabled','not built','aborted'])
+    #@total_jobs_by_jenkins_master = Gchart.bar(:data => [[
   end
 end
