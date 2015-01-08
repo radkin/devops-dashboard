6e3d291530251e49478af10a129f97081af9521a
Noel Miller <m673639@a4440486.federated.fds>
2015-01-08 15:10:56 -0800

    added Red vs Blue job chart to Jenkins

--- ddash/app/controllers/jenkins_jobs_controller.rb
+++ ddash/app/controllers/jenkins_jobs_controller.rb
@@ -1,6 +1,8 @@
 class JenkinsJobsController < ApplicationController
+  require 'googlecharts'
   def index
     @red_jobs     = JenkinsHello.by_color_red.uniq_job
     @blue_jobs    = JenkinsHello.by_color_blue.uniq_job
+    @pie_chart    = Gchart.pie(:data => [@red_jobs.count,@blue_jobs.count], :title => 'Red vs Blue jobs', :size => '400x200', :labels =>     ['red', 'blue'])
   end
 end
