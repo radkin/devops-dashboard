db8134f20cef4a6db426fc205885280eaefac2bf
Noel Miller <m673639@a4440486.federated.fds>
2015-01-09 13:44:08 -0800

    fixed some db schema bits and started adding by_master job listings. Unfortunately, it only shows one master at the moment. I suspect it is simply the last one and that there needs to be more logic to support a hash of masters jobs

--- ddash/app/controllers/jenkins_jobs_controller.rb
+++ ddash/app/controllers/jenkins_jobs_controller.rb
@@ -9,6 +9,11 @@ class JenkinsJobsController < ApplicationController
     @disabled_jobs  = JenkinsHello.by_color_disabled.uniq_job
     @notbuilt_jobs  = JenkinsHello.by_color_notbuilt.uniq_job
     @aborted_jobs   = JenkinsHello.by_color_aborted.uniq_job
+    # jobs by master
+    @masters_jobs = Hash.new
+    Ddash::Application.config.JENKINS_MASTERS.each do |master|
+      @masters_jobs = JenkinsHello.by_master
+    end
     # charts
     @total_job_status_pie_chart = Gchart.pie(:data => [@red_jobs.count,@blue_jobs.count,@yellow_jobs.count,@grey_jobs.count,@disabled_jobs.count,@notbuilt_jobs.count,@aborted_jobs.count], :title => 'Total job status', :size => '600x300', :labels =>     ['red','blue','yellow','grey','disabled','not built','aborted'])
     #@total_jobs_by_jenkins_master = Gchart.bar(:data => [[
