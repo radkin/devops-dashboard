2a2572e10cb4d087880ab9a7322bb11db7e21a7c
Noel Miller <m673639@a4440486.federated.fds>
2015-01-13 16:42:53 -0800

    added create action for updating Jenkins data. Now, the jenkins hello and updating Jenkins data live on the same page with a button.

--- ddash/app/controllers/jenkins_hello_controller.rb
+++ ddash/app/controllers/jenkins_hello_controller.rb
@@ -1,8 +1,45 @@
 class JenkinsHelloController < ApplicationController
   require 'resque'
+  require 'googlecharts'
   require_dependency 'reporter'
   require_dependency 'jenkins_jobs_objects'
   def index
+    # jobs
+    @red_jobs       = JenkinsHello.by_color_red.uniq_job
+    @blue_jobs      = JenkinsHello.by_color_blue.uniq_job
+    @yellow_jobs    = JenkinsHello.by_color_yellow.uniq_job
+    @grey_jobs      = JenkinsHello.by_color_grey.uniq_job
+    @disabled_jobs  = JenkinsHello.by_color_disabled.uniq_job
+    @notbuilt_jobs  = JenkinsHello.by_color_notbuilt.uniq_job
+    @aborted_jobs   = JenkinsHello.by_color_aborted.uniq_job
+    # jobs by master
+    @masters_jobs       = Hash.new
+    @masters_counts     = Array.new
+    @masters_names      = Array.new
+    @masters_aborted    = Array.new
+    @masters_red        = Array.new
+    @masters_blue       = Array.new
+    Ddash::Application.config.JENKINS_MASTERS.each do |master|
+      @masters_jobs[master] = JenkinsHello.by_master(master)
+      @masters_names.push(master)
+      @masters_counts.push(JenkinsHello.by_master(master).count)
+      @masters_aborted.push(JenkinsHello.by_master(master).by_color_aborted.count)
+      @masters_red.push(JenkinsHello.by_master(master).by_color_red.count)
+      @masters_blue.push(JenkinsHello.by_master(master).by_color_blue.count)
+    end
+    #### charts
+    # Total Jobs by status
+    @total_job_status_pie_chart = Gchart.pie_3d(:data => [@red_jobs.count,@blue_jobs.count,@yellow_jobs.count,@grey_jobs.count,@disabled_jobs.count,@notbuilt_jobs.count,@aborted_jobs.count], :title => 'Total job status', :size => '600x300', :labels =>     ['red','blue','yellow','grey','disabled','not built','aborted'])
+    # Jobs by Jenkins Master
+    @jobs_by_jenkins_master_chart = Gchart.pie(:data => @masters_counts, :title => 'Jobs by Jenkins Master', :size => '600x300', :labels =>     @masters_names)
+    # Failed Jobs by Jenkins Master
+    @aborted_jobs_by_jenkins_master_chart = Gchart.pie(:data => @masters_aborted, :title => 'Aborted Jobs by Jenkins Master', :size => '600x300', :labels =>     @masters_names)
+    # red Jobs by Jenkins Master
+    @red_jobs_by_jenkins_master_chart = Gchart.pie(:data => @masters_red, :title => 'Red Jobs by Jenkins Master', :size => '600x300', :labels =>     @masters_names)
+    # blue Jobs by Jenkins Master
+    @blue_jobs_by_jenkins_master_chart = Gchart.pie(:data => @masters_blue, :title => 'Blue Jobs by Jenkins Master', :size => '600x300', :labels =>     @masters_names)
+  end
+  def create
     @all_status       = []
     kaboose           = "api/json"
     gen_report        = Reporter.new
@@ -22,6 +59,4 @@ class JenkinsHelloController < ApplicationController
       end
     end
   end
-  def show
-  end
-end
+end # end class
