a318273a81175347090f4e8154cb57e12ee85a3b
radkin <noeldamonmiller@gmail.com>
2020-02-26 11:47:09 -0800

    master fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'jenkins_hello#index'
+  get 'jenkins_hello' => 'jenkins_hello#index'
+  post 'jenkins_hello/create' => 'jenkins_hello#create'
 end
--- lib/jenkins_masters.rb
+++ lib/jenkins_masters.rb
@@ -1,18 +1,10 @@
 # All things related to Jenkins Masters
 class JenkinsMasters
-  if defined?(j_masters_str)
-    def generate
-      j_masters_str     = ENV['JENKINS_MASTERS']
-      @jenkins_masters  = []
-      j_masters_str.split(/,/).each do |master|
-        @jenkins_masters.push(*master)
-      end
-      @jenkins_masters
-    end
-  # if we haven't set the environment variable use localhost as a dummy value  
-  else
-    def generate
-      @jenkins_masters = 'localhost'
+  def generate
+    j_masters_str     = ENV['JENKINS_MASTERS']
+    @jenkins_masters  = []
+    j_masters_str.split(/,/).each do |master|
+      @jenkins_masters.push(*master)
     end
     @jenkins_masters
   end
