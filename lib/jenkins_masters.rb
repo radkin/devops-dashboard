9f317b7e66d2f98478035520911a818367bee95a
radkin <noeldamonmiller@gmail.com>
2020-02-26 08:53:20 -0800

    master added a dummy value for localhost
    this should allow the split method to be happily splitting an array of 1
    value

--- lib/jenkins_masters.rb
+++ lib/jenkins_masters.rb
@@ -1,10 +1,18 @@
 # All things related to Jenkins Masters
 class JenkinsMasters
-  def generate
-    j_masters_str     = ENV['JENKINS_MASTERS']
-    @jenkins_masters  = []
-    j_masters_str.split(/,/).each do |master|
-      @jenkins_masters.push(*master)
+  if defined?(j_masters_str)
+    def generate
+      j_masters_str     = ENV['JENKINS_MASTERS']
+      @jenkins_masters  = []
+      j_masters_str.split(/,/).each do |master|
+        @jenkins_masters.push(*master)
+      end
+      @jenkins_masters
+    end
+  # if we haven't set the environment variable use localhost as a dummy value  
+  else
+    def generate
+      @jenkins_masters = 'localhost'
     end
     @jenkins_masters
   end
